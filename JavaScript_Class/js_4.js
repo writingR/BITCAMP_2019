5df39667aab26791d69130b0ec0b1e223fd2d57d
writingR <writing0625@gmail.com>
2019-06-10 23:57:32 +0900

    javascript _ function (sum,sub,multi,div,mod)

---
+++ JavaScript_Class/js_4.js
@@ -0,0 +1,26 @@
+// 함수 호출하기
+
+
+function sum(a,b){
+    return a+b;
+}
+function sub(c,d){
+    return c-d;
+}
+function multi(e,f){
+    return e*f;
+}
+function div(g,h){
+    return g/h;
+}
+function mod(i,j){
+    return i%j;
+}
+
+
+alert(sum(10,9));
+alert(sub(8,1));
+alert(multi(3,4));    
+alert(div(6,3));
+alert(mod(4,3));
+
